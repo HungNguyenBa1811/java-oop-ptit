afc52dc38e234a27eb530099cdaa5dde65721837
HungNguyenBa1811 <nahh1811@gmail.com>
2025-10-07 11:22:26 +0700

    refactor(app-folder): Rearrange folder for convention

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import view.AppView;
+
 public class Main {
     public static void main(String[] args) {
-        core.AppView.main(args);
+        AppView.main(args);
     }
 }
--- src/view/AppView.java
+++ src/view/AppView.java
@@ -1,4 +1,4 @@
-package core;
+package view;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -16,14 +16,16 @@ public class AppView extends Application {
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
+        loadFonts();
         // Stage stage = new Stage();
+
         Group root = new Group();
         Scene scene = new Scene(root, 800, 600, Color.BLACK);
 
         Text text = new Text();
         text.setText("Huzano 2.0 App");
         text.setY(100);
-        text.setFont(Font.font("Comic Sans MS", 50));
+        text.setFont(Font.font("Comic Sans Pro Regular", 50));
         text.setFill(Color.WHITE);
 
         // Auto Alignment
@@ -36,7 +38,7 @@ public class AppView extends Application {
 
         root.getChildren().add(text);
 
-        Image icon = new Image(AppView.class.getResourceAsStream("/assets/images/huzano.png"));
+        Image icon = new Image(AppView.class.getResourceAsStream("/resources/assets/images/huzano.png"));
         primaryStage.getIcons().add(icon);
         primaryStage.setTitle("Dung Huzano 2.0");
         // primaryStage.setWidth(800);
@@ -50,5 +52,22 @@ public class AppView extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    public static void loadFonts() {
+        final String RESET = "\u001B[0m";
+        final String GREEN = "\u001B[32m";
+        final String RED = "\u001B[31m";
+
+        String[] fonts = {
+            "/resources/assets/fonts/Comic Sans Pro W99 Bold Italic.ttf",
+            "/resources/assets/fonts/Comic Sans Pro W99 Bold.ttf",
+            "/resources/assets/fonts/Comic Sans Pro W99 Italic.ttf",
+            "/resources/assets/fonts/Comic Sans Pro W99 Regular.ttf",
+        };
+
+        for (String path : fonts) {
+            Font font = Font.loadFont(AppView.class.getResourceAsStream(path), 12);
+            System.out.println((font != null ? String.format("%sLoaded: %s | Family: %s%s", GREEN, font.getName(), font.getFamily(), RESET) : String.format("%sFailed: %s%s", RED, path, RESET)));
+        }
     }
 }
