63b474e283fd6bf329621b538c30201dfe81a63d
vamcrizer <vamcrizermcgaming@gmail.com>
2025-10-09 10:20:57 +0700

    feat: Updated User.java

---
+++ src/main/java/model/User.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class User{
+    private String userId;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private boolean role;
+
+    protected User(){}
+
+    public User
+}
